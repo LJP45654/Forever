2bf73848ca21c62c44ef8040d76b9a66d9e1192e
LJP45654 <934421082@qq.com>
2025-07-29 10:37:49 +0800

    ticker name,currency type query api

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getAllCashRecords } from '../controllers/cashController.js';
+import { getAllCashRecords, getCashCurrency} from '../controllers/cashController.js';
+import { getTickerNames } from '../controllers/stockController.js';
 
 const router = Router();
 
 // 现金记录路由
 router.get('/cash', getAllCashRecords);
+router.get('/cash/currency', getCashCurrency);
+
+// 股票路由
+router.get('/stock/name', getTickerNames);
 
 export default router;
