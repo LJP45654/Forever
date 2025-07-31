47092b19ff61094bdc4af6c0d18417d0849425f9
LJP45654 <934421082@qq.com>
2025-07-31 12:48:18 +0800

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/routes.js');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = 3002;
 
